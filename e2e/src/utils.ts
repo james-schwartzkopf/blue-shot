025612e399d86b59bfae90761161289233056858
James Schwartzkopf <james.schwartzkopf@eagletg.com>
2019-02-19 19:32:56 -0600

    meta viewport 3

--- e2e/src/utils.ts
+++ e2e/src/utils.ts
@@ -15,7 +15,7 @@ export async function findViewportInBrowserChrome(browser: WebDriver) {
     <!DOCTYPE html>
     <html lang="en">
       <head>
-        <meta name="viewport" content="initial-scale=1">
+        <meta name="viewport" content="width=device-width">
         <title>title</title>
       </head>
       <body style="background-color:#F00">
