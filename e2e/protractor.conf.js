ccfa47a2fa0f27a2eea1be0382cf47ebf7d45981
James Schwartzkopf <james.schwartzkopf@eagletg.com>
2019-02-21 21:21:26 -0600

    way to much munging.3

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -42,7 +42,7 @@ exports.config = {
     print: function() {}
   },
   beforeLaunch() {
-    const {express} = require('express');
+    const express = require('express');
     const serveStatic = require('serve-static');
     const expressModifyResponse = require('express-modify-response');
 
