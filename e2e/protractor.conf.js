3161e71157af2f0a55c0498cd114b16d0b85bed5
James Schwartzkopf <james.schwartzkopf@eagletg.com>
2019-03-05 22:48:43 -0600

    maybe more time will help

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -134,7 +134,7 @@ exports.config = {
 
     //Pause so Safari has time to hide it's scroll bars.
     if (browserName === 'safari') {
-      setPauseBeforeScreenshot(3000);
+      setPauseBeforeScreenshot(5000);
     }
 
     //used for image paths, etc.
