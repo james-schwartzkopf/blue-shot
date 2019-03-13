0ddbc432f6aff3829757f4a5c4e3a12dd1299ad7
James Schwartzkopf <james.schwartzkopf@eagletg.com>
2019-03-13 01:51:58 -0500

    Clip viewport maybe?

--- lib/index.ts
+++ lib/index.ts
@@ -418,7 +418,7 @@ export function transformCaptureScreenRegionFactory(
 }
 function buildCaptureScreenRegion(browser: WebDriver): CaptureContentRectInto {
   const captureScreenRegion = captureScreenRegionFactory(browser) as CaptureContentRectInto;
-  captureScreenRegion.clipRect = {top: 5, left: 0, height: Number.MAX_VALUE, width: Number.MAX_VALUE};
+  captureScreenRegion.clipRect = {top: 0, left: 0, height: Number.MAX_VALUE, width: Number.MAX_VALUE};
   return captureScreenRegion;
 }
 
